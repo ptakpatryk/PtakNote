b8a6475bd4204ec70255c480b99453296a3307bf
Patryk Ptak <ptaszqq@gmail.com>
2020-05-05 08:56:48 +0100

    Updates

---
+++ precache-manifest.7e6eac1d02dbe644054716116a6c6c5a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0d150c21569522c95011de4678188f",
+    "url": "/PtakNote/index.html"
+  },
+  {
+    "revision": "1b0a03a04efb7a888199",
+    "url": "/PtakNote/static/js/2.3b2ca90b.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/PtakNote/static/js/2.3b2ca90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad61110d2f20dcafd00",
+    "url": "/PtakNote/static/js/main.1cce1d15.chunk.js"
+  },
+  {
+    "revision": "e0bcf36baceeccc01670",
+    "url": "/PtakNote/static/js/runtime-main.e391694c.js"
+  },
+  {
+    "revision": "af737b5505b4cdb4a433cc3ac1f234af",
+    "url": "/PtakNote/static/media/bulb.af737b55.svg"
+  },
+  {
+    "revision": "6fc1d8fe5a3f17e73f1ecd0d757475d1",
+    "url": "/PtakNote/static/media/link.6fc1d8fe.svg"
+  },
+  {
+    "revision": "a91cdf6fac6fa12db8803e1777d64e15",
+    "url": "/PtakNote/static/media/logo.a91cdf6f.svg"
+  },
+  {
+    "revision": "0969c92b498df082b2f18fc455b2fa13",
+    "url": "/PtakNote/static/media/logout.0969c92b.svg"
+  },
+  {
+    "revision": "a14461b86078261a696a425d13ffe612",
+    "url": "/PtakNote/static/media/magnifier.a14461b8.svg"
+  },
+  {
+    "revision": "84b70f8a7155a0edb0576db0efc9ace2",
+    "url": "/PtakNote/static/media/pen.84b70f8a.svg"
+  },
+  {
+    "revision": "e43ce6c85c8383cb4442972a422cf804",
+    "url": "/PtakNote/static/media/plus.e43ce6c8.svg"
+  },
+  {
+    "revision": "051af8ba4d1984fc498ff6da805311cb",
+    "url": "/PtakNote/static/media/twitter.051af8ba.svg"
+  }
+]);
