323e37ffad4419d04398faa3b618d2c4f25bc24d
Patryk Ptak <ptaszqq@gmail.com>
2020-05-05 09:47:01 +0100

    redux devtools delete

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import authReducer from './reducer/auth';
 import appContent from './reducer/appContent';
@@ -7,11 +7,11 @@ const rootReducer = combineReducers({
   app: appContent,
   auth: authReducer,
 });
-const composeEnhancers =
-  // eslint-disable-next-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const devTools =
+//   // eslint-disable-next-line no-underscore-dangle
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 /* eslint-disable no-underscore-dangle */
-const store = createStore(rootReducer, compose(applyMiddleware(thunk), composeEnhancers));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
